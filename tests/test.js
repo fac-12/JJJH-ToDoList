63b6f81bfc6799edd16722659f69841b28f900bb
Johanna-hub <jyherman@gmail.com>
2017-11-01 13:00:19 +0000

    Addtodo function and tests

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,27 @@
 var test = require('tape');
-var logic = require('./logic');
+var logic = require('../logic');
 
-test('Example test', function(t) {
-  t.pass();
+test('Addtodo function', function(t) {
+  t.equals(typeof logic.addTodo(toDoExample, newDo), "object", "Addtodo returns an object");
+  t.equals(logic.addTodo(toDoExample, newDo).length, toDoExample.length + 1, "Addtodo adds Todo to newArr");
+  t.notDeepEqual(logic.addTodo(toDoExample, newDo), toDoExample, "newArr should not equal todos")
+  t.equals(logic.addTodo(toDoExample, newDo).pop().hasOwnProperty('id'), true, "id has been generated" )
+
   t.end();
 });
+
+var toDoExample = [{
+    id: 0,
+    description: 'smash avocados',
+    done: true
+  },
+  {
+    id: 1,
+    description: 'make coffee',
+    done: false
+  }
+];
+
+var newDo = {
+  description: 'go shopping'
+};
