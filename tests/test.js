24b46dde4e3b39c5ad94bc9373192164ae8e1d00
hannahcmtucker <hannahcmtucker@gmail.com>
2017-11-01 14:11:28 +0000

    Add deleteTodo function

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,29 @@
 var test = require('tape');
-var logic = require('./logic');
+var logic = require('../logic');
 
-test('Example test', function(t) {
-  t.pass();
-  t.end();
+
+
+test('Testing DeleteTodo function', function(t) {
+    t.equal(typeof logic.deleteTodo(toDoExample, 1), "object", "Should return an array");
+    t.notEquals(logic.deleteTodo(toDoExample, 1), logic.deleteTodo(toDoExample, 1), "Check that function does not return same array as input");
+    t.deepEquals(logic.deleteTodo(toDoExample, 1), toDoExampleDeleted, "Output array should not contain todo with an id of idToDelete");
+    t.end();
 });
+
+var toDoExample = [{
+        id: 0,
+        description: 'smash avocados',
+        done: true,
+    },
+    {
+        id: 1,
+        description: 'make coffee',
+        done: false,
+    },
+];
+
+var toDoExampleDeleted = [{
+    id: 0,
+    description: 'smash avocados',
+    done: true,
+}];
